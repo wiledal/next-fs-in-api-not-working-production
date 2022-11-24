2e6237d24414fd7fde180dce1cca0a7b397e012b
Hugo Wiledal <hugo.wiledal@gmail.com>
2022-11-24 13:33:51 +0100

    add api

---
+++ pages/api/load-from-data.ts
@@ -0,0 +1,17 @@
+import fs from "fs/promises";
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type Data = {
+  name: string;
+};
+
+const handler = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  const r = await fs
+    .readFile(process.cwd() + "/data/some-file.json", "utf8")
+    .then((p) => JSON.parse(p));
+
+  res.status(200).json(r);
+};
+
+export default handler;
